05043411fcb6fff2e36e72c75bd047cd430e4313
oleg.bondarenko <mnkrp.ml@gmail.com>
2025-03-11 21:32:11 +0100

    replaced in-memory zone storage with filesystem

--- apps/server/src/app/zone.repo.ts
+++ apps/server/src/app/zone.repo.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { Zone, ZoneData } from '@tetragons/shared';
 import { v4 as uuid } from 'uuid';
+import { mkdir, readdir, readFile, unlink, writeFile } from 'node:fs/promises';
+import path from 'node:path';
+import { setTimeout } from 'node:timers/promises';
 
 const delayMs = 1500;
 
@@ -10,78 +13,49 @@ function clone<T>(obj: T): T {
 
 @Injectable()
 export class ZoneRepository {
-  private state: Zone[] = [
-    {
-      id: '1',
-      name: 'Zone 1',
-      points: [
-        [12.3, 12.0],
-        [16.3, 12.0],
-        [16.3, 8.0],
-        [11.4, 8.7],
-      ],
-    },
-    {
-      id: '2',
-      name: 'Zone 2',
-      points: [
-        [-10.5, 9.0],
-        [-14.5, 9.0],
-        [-14.5, 5.0],
-        [-9.6, 5.7],
-      ],
-    },
-    {
-      id: '3',
-      name: 'Zone 3',
-      points: [
-        [-11.0, -7.5],
-        [-15.0, -7.5],
-        [-15.0, -11.5],
-        [-10.1, -10.8],
-      ],
-    },
-    {
-      id: '4',
-      name: 'Zone 4',
-      points: [
-        [9.8, -8.2],
-        [13.8, -8.2],
-        [13.8, -12.2],
-        [8.9, -11.5],
-      ],
-    },
-    {
-      id: '5',
-      name: 'Zone 5',
-      points: [
-        [1.0, 1.5],
-        [3.0, 1.5],
-        [3.0, -0.5],
-        [0.5, -0.2],
-      ],
-    },
-  ];
+  private readonly dataPath = './data';
 
-  public all(): Promise<Zone[]> {
-    return new Promise((res) =>
-      setTimeout(() => res(clone(this.state)), delayMs)
-    );
+  constructor() {
+    mkdir(path.resolve(this.dataPath), { recursive: true });
   }
 
-  public create(data: ZoneData): Promise<Zone> {
+  public async all(): Promise<Zone[]> {
+    const names = await readdir(path.resolve(this.dataPath));
+
+    const contents: string[] = await Promise.all(
+      names.map((one) =>
+        readFile(path.resolve(this.dataPath, one), { encoding: 'utf8' })
+      )
+    );
+
+    await this.delay();
+
+    return contents.map((text) => JSON.parse(text) as Zone);
+  }
+
+  public async create(data: ZoneData): Promise<Zone> {
     const zone = Object.assign(clone(data), {
       id: uuid(),
     });
 
-    this.state.push(zone);
+    await writeFile(
+      path.resolve(this.dataPath, zone.id),
+      JSON.stringify(zone),
+      { flag: 'w+' }
+    );
 
-    return new Promise((res) => setTimeout(() => res(clone(zone)), delayMs));
+    await this.delay();
+
+    return zone;
   }
 
-  public delete(id: string): Promise<void> {
-    this.state = this.state.filter((item) => item.id !== id);
+  public async delete(id: string): Promise<void> {
+    await this.delay();
 
-    return new Promise((res) => setTimeout(() => res(), delayMs));
+    return unlink(path.resolve(this.dataPath, id));
+  }
+
+  private async delay(): Promise<void> {
+    return setTimeout(delayMs);
   }
 }
